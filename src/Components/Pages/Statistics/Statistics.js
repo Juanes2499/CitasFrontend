d2a7ee71afee34125c3669a8fbed44411fa98984
Nestor Martinez <nestormartinez19999@gmail.com>
2020-11-23 21:58:15 -0500

    MÃore functions Added

--- src/Components/Pages/Statistics/Statistics.js
+++ src/Components/Pages/Statistics/Statistics.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Line } from "react-chartjs-2";
 import { faChartLine } from "@fortawesome/free-solid-svg-icons";
 import Accordion from "../../Elements/Accordion/Accordion3";
-
+import axios from "axios";
 
 class Statistics extends Component {
     constructor(props) {
@@ -11,8 +11,7 @@ class Statistics extends Component {
         this.update = this.update.bind(this);
         this.state = {
             selectedNode: "",
-            optionNodes: [
-                { node: 1 }, { node: 2 }, { node: 3 }, { node: 4 }]
+            optionNodes: []
             ,
             year: "", month: "", day: "", node: "",
             test: [{
@@ -84,18 +83,56 @@ class Statistics extends Component {
             dataRiverHeight: { labels: [], datasets: [] },
             dataFlow: { labels: [], datasets: [] },
             dataCaudal: { labels: [], datasets: [] },
+            status: false,
+            
         }
+        this.httpInstance = axios.create({
+            baseURL: "https://xme9h9w868.execute-api.us-east-1.amazonaws.com/get1",
+            timeout: 10000,
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+    
+    componentDidMount() {
+
+
+        this.httpInstance.get('/getnodes').then(respuesta => {
+            if (respuesta.status === 200) {
+                // console.log(respuesta.data.body);
+                this.setState({ criticalNodes: respuesta.data.body.length });
+                var result = []
+                for (var i in respuesta.data.body) {
+                    result.push({ node: respuesta.data.body[i].NumNodo });
+                }
+                this.setState({ optionNodes: result });
+                this.setState({ status: true });
+
+
+            } else {
+                console.log(respuesta);
+            }
+        });
+
     }
     componentDidMount() {
-        this.setState = {
-            dataAmbientTemperature: { labels: [], datasets: this.state.dataLine.datasets },
-            dataHumidity: { labels: [], datasets: this.state.dataLine.datasets },
-            dataWindSpeed: { labels: [], datasets: this.state.dataLine.datasets },
-            dataRiverHeight: { labels: [], datasets: this.state.dataLine.datasets },
-            dataRiverTemperature: { labels: [], datasets: this.state.dataLine.datasets },
-            dataFlow: { labels: [], datasets: this.state.dataLine.datasets },
-            dataCaudal: { labels: [], datasets: this.state.dataLine.datasets }
-        }
+
+
+        this.httpInstance.get('/getnodes').then(respuesta => {
+            if (respuesta.status === 200) {
+                // console.log(respuesta.data.body);
+                var result = []
+                for (var i in respuesta.data.body) {
+                    result.push({ node: respuesta.data.body[i].NumNodo });
+                }
+                this.setState({ optionNodes: result });
+                this.setState({ status: true });
+
+
+            } else {
+                console.log(respuesta);
+            }
+        });
+
     }
     update = (name, e) => {
         this.setState({ [name]: e.target.value });
@@ -113,106 +150,112 @@ class Statistics extends Component {
         console.log(this.state.dataAmbientTemperature.datasets[0].data)
     }
     render() {
-
-        return (
-            <div className="p-2 mb-4">
-                <div className="row  mt-0">
-                    <p className="h5 font-weight-bold mt-0 text-muted">Bienvenido a Smart Cities UAO</p>
-                </div>
-                <div className="row  d-flex justify-content-center">
-                    <div className="col-sm-10">
-                        <div className="card p-0">
-                            <div className="card-header bg-primary text-white d-flex ">
-                                <p className="m-0 align-middle">Tabla de resultados por nodo</p>
-                            </div>
-                            <div className="card-body">
-                                <div className="row p-1">
-                                    <div className="col-4 col-sm-2 ">
-                                        <label className="sr-only" htmlFor="inlineFormInputGroup">Año</label>
-                                        <div className="input-group mb-2">
-                                            <div className="input-group-prepend">
-                                                <div className="input-group-text">Año</div>
+        if (this.state.status) {
+            return (
+                <div className="p-2 mb-4">
+                    <div className="row  mt-0">
+                        <p className="h5 font-weight-bold mt-0 text-muted">Bienvenido a Smart Cities UAO</p>
+                    </div>
+                    <div className="row  d-flex justify-content-center">
+                        <div className="col-sm-10">
+                            <div className="card p-0">
+                                <div className="card-header bg-primary text-white d-flex ">
+                                    <p className="m-0 align-middle">Tabla de resultados por nodo</p>
+                                </div>
+                                <div className="card-body">
+                                    <div className="row p-1">
+                                        <div className="col-4 col-sm-2 ">
+                                            <label className="sr-only" htmlFor="inlineFormInputGroup">Año</label>
+                                            <div className="input-group mb-2">
+                                                <div className="input-group-prepend">
+                                                    <div className="input-group-text">Año</div>
+                                                </div>
+                                                <input type="text" onChange={(e) => this.update("year", e)} value={this.state.year} className="form-control" />
                                             </div>
-                                            <input type="text" onChange={(e) => this.update("year", e)} value={this.state.year} className="form-control" />
                                         </div>
-                                    </div>
-                                    <div className="col-4 col-sm-2 ">
-                                        <label className="sr-only" htmlFor="inlineFormInputGroup">Mes</label>
-                                        <div className="input-group mb-2">
-                                            <div className="input-group-prepend">
-                                                <div className="input-group-text">Mes</div>
+                                        <div className="col-4 col-sm-2 ">
+                                            <label className="sr-only" htmlFor="inlineFormInputGroup">Mes</label>
+                                            <div className="input-group mb-2">
+                                                <div className="input-group-prepend">
+                                                    <div className="input-group-text">Mes</div>
+                                                </div>
+                                                <input type="text" onChange={(e) => this.update("month", e)} value={this.state.month} className="form-control" />
                                             </div>
-                                            <input type="text" onChange={(e) => this.update("month", e)} value={this.state.month} className="form-control" />
                                         </div>
-                                    </div>
-                                    <div className="col-4 col-sm-2 ">
-                                        <label className="sr-only" htmlFor="inlineFormInputGroup">Día</label>
-                                        <div className="input-group mb-2">
-                                            <div className="input-group-prepend">
-                                                <div className="input-group-text">Día</div>
+                                        <div className="col-4 col-sm-2 ">
+                                            <label className="sr-only" htmlFor="inlineFormInputGroup">Día</label>
+                                            <div className="input-group mb-2">
+                                                <div className="input-group-prepend">
+                                                    <div className="input-group-text">Día</div>
+                                                </div>
+                                                <input type="text" onChange={(e) => this.update("day", e)} value={this.state.day} className="form-control" />
                                             </div>
-                                            <input type="text" onChange={(e) => this.update("day", e)} value={this.state.day} className="form-control" />
                                         </div>
-                                    </div>
-                                    <div className="col-4 col-sm-3 ">
-                                        <select value={this.state.selectedNode} onChange={(e) => this.update("selectedNode", e)} className="custom-select" id="nodeSelector">
-                                            <option defaultValue>Todos los nodos</option>
-                                            {this.state.optionNodes.map((option, i) => {
-                                                return (
-                                                    <option key={i} value={option.node}>Nodo {option.node}</option>
-                                                );
-                                            })}
-                                        </select>
-                                    </div>
-                                    <div className="col col-sm-1">
-                                        <button onClick={this.search} className="btn btn-success p-2 text-white z-depth-0 m-0 text-right align-middle mr-auto">Consultar</button>
+                                        <div className="col-4 col-sm-3 ">
+                                            <select value={this.state.selectedNode} onChange={(e) => this.update("selectedNode", e)} className="custom-select" id="nodeSelector">
+                                                <option defaultValue>Todos los nodos</option>
+                                                {this.state.optionNodes.map((option, i) => {
+                                                    return (
+                                                        <option key={i} value={option.node}>Nodo {option.node}</option>
+                                                    );
+                                                })}
+                                            </select>
+                                        </div>
+                                        <div className="col col-sm-1">
+                                            <button onClick={this.search} className="btn btn-success p-2 text-white z-depth-0 m-0 text-right align-middle mr-auto">Consultar</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="row mt-4">
-                    <div className="col-sm-6">
-                        <Accordion title="Temperatura del ambiente" color="bg-danger" icon={faChartLine}>
-                            <Line data={this.state.dataLine} options={{ responsive: true }} />
-                        </Accordion>
+                    <div className="row mt-4">
+                        <div className="col-sm-6">
+                            <Accordion title="Temperatura del ambiente" color="bg-danger" icon={faChartLine}>
+                                <Line data={this.state.dataLine} options={{ responsive: true }} />
+                            </Accordion>
+                        </div>
+                        <div className="col-sm-6">
+                            <Accordion title="Humedad" color="bg-primary" icon={faChartLine}>
+                                <Line data={this.state.dataLine} options={{ responsive: true }} />
+                            </Accordion>
+                        </div>
+                        <div className="col-sm-6">
+                            <Accordion title="Velocidad el viento" color="bg-warning" icon={faChartLine}>
+                                <Line data={this.state.dataLine} options={{ responsive: true }} />
+                            </Accordion>
+                        </div>
+                        <div className="col-sm-6">
+                            <Accordion title="Temperatura del río" color="bg-success" icon={faChartLine}>
+                                <Line data={this.state.dataLine} options={{ responsive: true }} />
+                            </Accordion>
+                        </div>
+                        <div className="col-sm-6">
+                            <Accordion title="Nivel del río" color="bg-secondary" icon={faChartLine}>
+                                <Line data={this.state.dataLine} options={{ responsive: true }} />
+                            </Accordion>
+                        </div>
+                        <div className="col-sm-6">
+                            <Accordion title="Flujo" color="default-color-dark" icon={faChartLine}>
+                                <Line data={this.state.dataLine} options={{ responsive: true }} />
+                            </Accordion>
+                        </div>
+                        <div className="col-sm-6">
+                            <Accordion title="Caudal" color="amber darken-2" icon={faChartLine}>
+                                <Line data={this.state.dataLine} options={{ responsive: true }} />
+                            </Accordion>
+                        </div>
                     </div>
-                    <div className="col-sm-6">
-                        <Accordion title="Humedad" color="bg-primary" icon={faChartLine}>
-                            <Line data={this.state.dataLine} options={{ responsive: true }} />
-                        </Accordion>
-                    </div>
-                    <div className="col-sm-6">
-                        <Accordion title="Velocidad el viento" color="bg-warning" icon={faChartLine}>
-                            <Line data={this.state.dataLine} options={{ responsive: true }} />
-                        </Accordion>
-                    </div>
-                    <div className="col-sm-6">
-                        <Accordion title="Temperatura del río" color="bg-success" icon={faChartLine}>
-                            <Line data={this.state.dataLine} options={{ responsive: true }} />
-                        </Accordion>
-                    </div>
-                    <div className="col-sm-6">
-                        <Accordion title="Nivel del río" color="bg-secondary" icon={faChartLine}>
-                            <Line data={this.state.dataLine} options={{ responsive: true }} />
-                        </Accordion>
-                    </div>
-                    <div className="col-sm-6">
-                        <Accordion title="Flujo" color="default-color-dark" icon={faChartLine}>
-                            <Line data={this.state.dataLine} options={{ responsive: true }} />
-                        </Accordion>
-                    </div>
-                    <div className="col-sm-6">
-                        <Accordion title="Caudal" color="amber darken-2" icon={faChartLine}>
-                            <Line data={this.state.dataLine} options={{ responsive: true }} />
-                        </Accordion>
-                    </div>
-                </div>
 
 
-            </div>
-        )
+                </div>
+            )
+        } else {
+            return (
+                <div>Cargando...</div>
+            )
+        }
+        
     }
 
 }
